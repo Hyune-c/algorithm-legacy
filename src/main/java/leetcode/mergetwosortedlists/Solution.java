6f9328348f5da7880493f757d32ab4d02678858f
Byung Hyun Choi <choi8608@gmail.com>
2020-01-18 02:40:19 +0900

    update

--- src/main/java/leetcode/mergetwosortedlists/Solution.java
+++ src/main/java/leetcode/mergetwosortedlists/Solution.java
@@ -1,34 +1,19 @@
 package leetcode.mergetwosortedlists;
 
 public class Solution {
-    static void insertNode(ListNode node, int input) {
-        if (node.next == null) {
-            node.next = new ListNode(input);
-            return;
-        } else {
-            insertNode(node.next, input);
-        }
-    }
-
-    static ListNode initListNode(int[] input) {
-        ListNode headNode = new ListNode(input[0]);
-
-        for (int i = 1; i < input.length; i++) insertNode(headNode, input[i]);
-
-        return headNode;
-    }
-
     static ListNode mergeSort(ListNode l1, ListNode l2) {
         ListNode l1HeadNode = l1;
         ListNode l2HeadNode = l2;
         ListNode returnNNodeNow = null;
         ListNode returnHeadNode = null;
 
+        if (l1HeadNode == null && l2HeadNode == null) return null;
+
         if (l1HeadNode != null && l2HeadNode == null) {
             returnHeadNode = new ListNode(l1HeadNode.val);
             returnNNodeNow = returnHeadNode;
             l1HeadNode = l1HeadNode.next;
-        } else if (l1HeadNode != null && l2HeadNode == null) {
+        } else if (l1HeadNode == null && l2HeadNode != null) {
             returnHeadNode = new ListNode(l2HeadNode.val);
             returnNNodeNow = returnHeadNode;
             l2HeadNode = l2HeadNode.next;
@@ -43,7 +28,6 @@ public class Solution {
         } else {
             System.out.println("*** Wrong");
         }
-
 
         while (!(l1HeadNode == null && l2HeadNode == null)) {
             if (l1HeadNode != null && l2HeadNode == null) {
@@ -74,14 +58,5 @@ public class Solution {
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         return mergeSort(l1, l2);
-    }
-
-    public static void main(String[] args) {
-        int[] input = new int[]{1, 2, 4};
-        int[] input2 = new int[]{1, 3, 4};
-        ListNode l1 = initListNode(input);
-        ListNode l2 = initListNode(input2);
-
-        mergeTwoLists(l1, l2);
     }
 }
--- src/test/java/leetcode/mergetwosortedlists/SolutionTest.java
+++ src/test/java/leetcode/mergetwosortedlists/SolutionTest.java
@@ -10,17 +10,73 @@ class SolutionTest {
     Solution sol;
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach() {
         sol = new Solution();
     }
 
+    static void insertNode(ListNode node, int input) {
+        if (node.next == null) {
+            node.next = new ListNode(input);
+            return;
+        } else {
+            insertNode(node.next, input);
+        }
+    }
+
+    static ListNode initListNode(int[] input) {
+        ListNode headNode = null;
+        try {
+            headNode = new ListNode(input[0]);
+            for (int i = 1; i < input.length; i++) insertNode(headNode, input[i]);
+        } catch (Exception e) {
+
+        } finally {
+            return headNode;
+        }
+    }
+    
     @Test
     void mergeTwoLists() {
         int[] input = new int[]{1, 2, 4};
         int[] input2 = new int[]{1, 3, 4};
-        l1 = sol.initListNode(input);
-        l2 = sol.initListNode(input2);
+        l1 = initListNode(input);
+        l2 = initListNode(input2);
 
         returnHeadNode = sol.mergeSort(l1, l2);
+
+        System.out.println();
+    }
+
+    @Test
+    void mergeTwoLists2() {
+        int[] input = new int[]{1, 2, 4};
+        int[] input2 = new int[]{};
+        l1 = initListNode(input);
+        l2 = initListNode(input2);
+
+        returnHeadNode = sol.mergeSort(l1, l2);
+        System.out.println();
+    }
+
+    @Test
+    void mergeTwoLists3() {
+        int[] input = new int[]{};
+        int[] input2 = new int[]{};
+        l1 = initListNode(input);
+        l2 = initListNode(input2);
+
+        returnHeadNode = sol.mergeSort(l1, l2);
+        System.out.println();
+    }
+
+    @Test
+    void mergeTwoLists4() {
+        int[] input = new int[]{};
+        int[] input2 = new int[]{0};
+        l1 = initListNode(input);
+        l2 = initListNode(input2);
+
+        returnHeadNode = sol.mergeSort(l1, l2);
+        System.out.println();
     }
 }
