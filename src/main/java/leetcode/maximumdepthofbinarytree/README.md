0fdcabece1ff874691bc061ce56ae614f42bf9a7
Byung Hyun Choi <choi8608@gmail.com>
2020-01-25 18:48:35 +0900

    update

--- src/main/java/leetcode/maximumdepthofbinarytree/README.md
+++ src/main/java/leetcode/maximumdepthofbinarytree/README.md
@@ -24,8 +24,7 @@ return its depth = 3.
 ## 풀이
 > Solution
 
-###### public ListNode getIntersectionNode(ListNode headA, ListNode headB) 
-- headA 의 리스트를 HashSet 으로 저장하고 headB 의 리스트를 순회하면서 같은 ListNode 가 나오면 리턴 합니다. 
+###### public int maxDepth(TreeNode root)  
 
 
 ## Test    
@@ -33,4 +32,3 @@ return its depth = 3.
 
 
 ## 그외
-또 다른 풀이로는 headA, headB 의 ListNodes 를 각각 만들고 length 를 비교하여 그 차 만큼을 이용하는 방법도 있었습니다. 
--- src/main/java/leetcode/maximumdepthofbinarytree/Solution.java
+++ src/main/java/leetcode/maximumdepthofbinarytree/Solution.java
@@ -1,19 +1,12 @@
 package leetcode.maximumdepthofbinarytree;
 
 class Solution {
-    public double inorder(TreeNode root, int depth) {
-        double leftDepth = 0, rightDepth = 0;
-        if (root.left != null)
-            leftDepth = inorder(root.left, depth + 1);
-
-        if (root.right != null)
-            rightDepth = inorder(root.right, depth + 1);
-
-        return Math.max(depth, Math.max(rightDepth, leftDepth));
-    }
-
     public int maxDepth(TreeNode root) {
         if (root == null) return 0;
-        return (int) inorder(root, 1);
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
     }
 }
