ee29ffd031da3f2255e4eaef0690e0f47c40b445
Byung Hyun Choi <choi8608@gmail.com>
2020-01-25 20:00:35 +0900

    update

--- src/main/java/leetcode/majorityelement/README.md
+++ src/main/java/leetcode/majorityelement/README.md
@@ -21,7 +21,9 @@ Output: 2
 ## 풀이
 > Solution
 
-###### public int maxDepth(TreeNode root)  
+###### public int majorityElement(int[] nums)  
+문제의 조건에 nums 배열은 비지 않고, majority 값이 항상 존재한다고 전제되어 있습니다.  
+이 배열을 정렬하면 그 개수가 2/n 보다 큰 수는 배열의 정중앙에 위치하게 됩니다. 
 
 
 ## Test    
--- src/main/java/leetcode/majorityelement/Solution.java
+++ src/main/java/leetcode/majorityelement/Solution.java
@@ -1,22 +1,10 @@
 package leetcode.majorityelement;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 class Solution {
     public int majorityElement(int[] nums) {
-        if (nums.length == 1) return nums[0];
-
-        Map<Integer, Integer> numCnt = new HashMap<>();
-
-        for (int num : nums) {
-            if (numCnt.containsKey(num)) {
-                int ret = numCnt.get(num) + 1;
-                if (ret > nums.length / 2) return num;
-                numCnt.put(num, ret);
-            } else numCnt.put(num, 1);
-        }
-
-        return 0;
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
     }
 }
