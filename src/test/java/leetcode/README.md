80d94d1a1d626fe373c7ee86c17ce879ed280c4a
Byung Hyun Choi <choi8608@gmail.com>
2020-01-28 09:07:26 +0900

    update

--- src/test/java/leetcode/README.md
+++ src/test/java/leetcode/README.md
@@ -12,5 +12,8 @@ nodaA 와 nodeB 를 headnode 로 가지는 linkedList 가 같은지 확인합니
 > Tree
 [symmetrictree](https://github.com/Hyune-c/algorithm/blob/master/src/test/java/leetcode/symmetrictree/SymmetrictreeTest.java)
   
-###### Node makeNodeTree(Node root, Integer[] arr, int i) 
+###### TreeNode makeNodeTree(TreeNode root, Integer[] arr, int i) 
 재귀적으로 NodeTree 를 만듭니다. i 값은 arr 배열의 index 로 초기 값은 0 입니다.  
+
+###### public boolean isSameTree(TreeNode nodeA, TreeNode nodeB)
+각 Tree 의 root 값을 받아 Tree 가 같은지 여부를 확인합니다.
--- src/test/java/leetcode/symmetrictree/SymmetrictreeTest.java
+++ src/test/java/leetcode/symmetrictree/SymmetrictreeTest.java
@@ -6,30 +6,37 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SymmetrictreeTest {
-    static class Node {
-        int value;
-        Node left;
-        Node right;
-
-        public Node(int value) {
-            this.value = value;
-            this.left = null;
-            this.right = null;
-        }
-    }
+//    public class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//
+//        TreeNode(int x) {
+//            val = x;
+//        }
+//    }
 
     Solution sol;
     Integer[] input;
     boolean expected;
 
-    Node makeNodeTree(Node root, Integer[] arr, int i) {
+    TreeNode makeNodeTree(TreeNode root, Integer[] arr, int i) {
         if (i < arr.length && arr[i] != null) {
-            root = new Node(arr[i]);
+            root = new TreeNode(arr[i]);
             root.left = makeNodeTree(root.left, arr, 2 * i + 1);
             root.right = makeNodeTree(root.right, arr, 2 * i + 2);
         }
 
         return root;
+    }
+
+    public boolean isSameTree(TreeNode nodeA, TreeNode nodeB) {
+        if (nodeA == null && nodeB == null) return true;
+        if (nodeA == null || nodeB == null) return false;
+        if (nodeA.val != nodeB.val) return false;
+
+        return isSameTree(nodeA.left, nodeB.left) && isSameTree(nodeA.right, nodeB.right);
+
     }
 
     @BeforeEach
@@ -40,10 +47,15 @@ class SymmetrictreeTest {
     @Test
     void isSymmetric() {
         input = new Integer[]{1, 2, 2, 3, 4, 4, 3};
+        Integer[] input2 = new Integer[]{1, 2, 2, 3, 4, 4, 3};
         expected = true;
 
-        Node root = null;
+        TreeNode root = null;
         root = makeNodeTree(root, input, 0);
 
+        TreeNode root2 = null;
+        root2 = makeNodeTree(root2, input2, 0);
+
+        assertTrue(isSameTree(root, root2));
     }
 }
