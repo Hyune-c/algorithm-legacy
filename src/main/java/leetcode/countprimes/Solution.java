dcf9a10e5453b850068359a579931754aeaa7d9e
Byung Hyun Choi <choi8608@gmail.com>
2020-02-04 21:09:44 +0900

    update

--- src/main/java/leetcode/countprimes/Solution.java
+++ src/main/java/leetcode/countprimes/Solution.java
@@ -8,12 +8,15 @@ class Solution {
         int[] arr = new int[n];
         int result = 0;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i * i < n; i++) {
             if (arr[i] == 1) continue;
-            result++;
-            for (int j = 1; i * j < n; j++) {
+            for (int j = 2; i * j < n; j++) {
                 arr[i * j] = 1;
             }
+        }
+
+        for (int i = 2; i < n; i++) {
+            if (arr[i] == 0) result++;
         }
 
         return result;
