1f4863537e22f3c115bc7cefd59e9541086605cf
Byung Hyun Choi <choi8608@gmail.com>
2020-02-05 14:48:32 +0900

    feat : Add '가운데 글자 가져오기' from programmers

---
+++ src/main/java/programmers/middleletter/Solution.java
@@ -0,0 +1,9 @@
+package programmers.middleletter;
+
+class Solution {
+    public String solution(String s) {
+        return (s.length() % 2 == 0)
+                ? "" + s.charAt((s.length() / 2) - 1) + s.charAt(s.length() / 2)
+                : "" + s.charAt(s.length() / 2);
+    }
+}
