42c1451089e03ec67926cdc88acce4f345cef4d7
Byung Hyun Choi <choi8608@gmail.com>
2020-02-05 13:07:17 +0900

    feat : Add '371. Sum of Two Integers'

---
+++ src/main/java/sumoftwointegers/Solution.java
@@ -0,0 +1,10 @@
+package sumoftwointegers;
+
+class Solution {
+    public int getSum(int a, int b) {
+        int s = a ^ b;
+        int carry = a & b;
+
+        return (carry == 0) ? s : getSum(s, carry << 1);
+    }
+}
