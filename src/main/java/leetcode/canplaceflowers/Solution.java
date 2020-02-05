8b7e18d53f34e0598128bccc4c1afea79a48356a
Byung Hyun Choi <choi8608@gmail.com>
2020-02-06 02:21:28 +0900

    refactor : 605. Can Place Flowers

--- src/main/java/leetcode/canplaceflowers/Solution.java
+++ src/main/java/leetcode/canplaceflowers/Solution.java
@@ -2,28 +2,26 @@ package leetcode.canplaceflowers;
 
 class Solution {
   public boolean canPlaceFlowers(int[] flowerbed, int n) {
-    int canPlaceCount = 0;
-
     if (flowerbed[0] == 0) {
       if (flowerbed.length == 1 || flowerbed[1] == 0) {
         flowerbed[0] = 1;
-        canPlaceCount++;
+        n--;
       }
     }
 
     for (int i = 1; i < flowerbed.length - 1; i++) {
       if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
         flowerbed[i] = 1;
-        canPlaceCount++;
+        n--;
       }
     }
 
     if (flowerbed[flowerbed.length - 1] == 0 && flowerbed.length != 1) {
       if (flowerbed[flowerbed.length - 2] == 0) {
-        canPlaceCount++;
+        n--;
       }
     }
 
-    return (canPlaceCount >= n) ? true : false;
+    return (n > 0) ? false : true;
   }
 }
