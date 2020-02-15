39e6f0c04a43b46d5c060c3b2d1710dc8bf2b649
Byung Hyun Choi <choi8608@gmail.com>
2020-02-15 19:22:06 +0900

    working

--- src/main/java/leetcode/longestpalindromicsubstring/Solution.java
+++ src/main/java/leetcode/longestpalindromicsubstring/Solution.java
@@ -26,10 +26,10 @@ class Solution {
 
     for (int i = 0; i < s.length(); i++) {
       for (int j = i + 1; j < s.length(); j++) {
-        if (s.charAt(i) == s.charAt(j)) {
-          if (isPalindrome(s.substring(i, j + 1))) {
-            result = (j - i > result.length()) ? s.substring(i, j + 1) : result;
-          }
+        if (s.charAt(i) == s.charAt(j)
+            && j - i + 1 > result.length()
+            && isPalindrome(s.substring(i, j + 1))) {
+          result = s.substring(i, j + 1);
         }
       }
     }
