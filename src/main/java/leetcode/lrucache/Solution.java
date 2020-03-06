97177e65d9b84c2af6b8811f9f488d21110c0404
Byung Hyun Choi <choi8608@gmail.com>
2020-03-06 20:35:14 +0900

    complete

--- src/main/java/leetcode/lrucache/Solution.java
+++ src/main/java/leetcode/lrucache/Solution.java
@@ -6,30 +6,23 @@ import java.util.Map;
 class LRUCache {
 
   private int capacity;
-  private Map<Integer, Integer> cacheMap = new LinkedHashMap<Integer, Integer>() {
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
-      return size() > capacity;
-    }
-  };
+  private Map<Integer, Integer> cacheMap;
 
   public LRUCache(int capacity) {
     this.capacity = capacity;
+    this.cacheMap = new LinkedHashMap<Integer, Integer>(capacity, 0.75F, true) {
+      @Override
+      protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
+      }
+    };
   }
 
   public int get(int key) {
-    int result = cacheMap.getOrDefault(key, -1);
-    if (result != -1) {
-      cacheMap.remove(key, result);
-      cacheMap.put(key, result);
-    }
-
-    return result;
+    return cacheMap.getOrDefault(key, -1);
   }
 
   public void put(int key, int value) {
-    cacheMap.put(key, value);
-    cacheMap.remove(key, value);
     cacheMap.put(key, value);
   }
 }
